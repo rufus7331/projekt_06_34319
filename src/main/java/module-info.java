2699b9caa470feec3405aa5677e6248b809c1521
Hubert <hubertos120@gmail.com>
2022-06-14 18:30:47 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.projekt_06_34319 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.projekt_06_34319 to javafx.fxml;
+    exports com.example.projekt_06_34319;
+}
