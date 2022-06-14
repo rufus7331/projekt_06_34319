cf8e378d40903d84c0614703b63da6469b94245c
Hubert <hubertos120@gmail.com>
2022-06-14 21:30:29 +0200

    dodanie progow punktowych i pytan do bazy

--- src/main/java/com/example/projekt_06_34319/Controller.java
+++ src/main/java/com/example/projekt_06_34319/Controller.java
@@ -9,9 +9,12 @@ import javafx.scene.layout.BorderPane;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Controller {
     public BorderPane pane;
+    @FXML
+    public Label result;
     @FXML
     private Label questionLabel;
     @FXML
@@ -46,6 +49,7 @@ public class Controller {
         rb3.setText(option3);
         rb4.setText(option4);
         submitButton.setText("Następne pytanie");
+        result.setText(" ");
 
 
         submitButton.setOnAction(event -> getNextQuestion(questions));
@@ -53,6 +57,9 @@ public class Controller {
     }
 
     private void getNextQuestion(List<Question> questions) {
+        submitButton.setText("Następne pytanie");
+
+
         if (currentQuestion<questions.size()) {
             String selectedRB = ((RadioButton)optionsGroup.getSelectedToggle()).getText();
             String correctAnswer = (String)questions.get(currentQuestion).getCorrectAns();
@@ -61,12 +68,11 @@ public class Controller {
                 System.out.println("Poprawna odpowiedź");
             }
             else {
-                score--;
                 System.out.println("Błędna odpowiedź");
             }
             currentQuestion++;
             if(currentQuestion<questions.size()){
-                questionLabel.setText(((Question)questions.get(currentQuestion)).getQuestion());
+                questionLabel.setText(questions.get(currentQuestion).getQuestion());
                 String option1 = (String)questions.get(currentQuestion).getOptions().get(0);
                 String option2 = (String)questions.get(currentQuestion).getOptions().get(1);
                 String option3 = (String)questions.get(currentQuestion).getOptions().get(2);
@@ -78,11 +84,38 @@ public class Controller {
                 rb4.setText(option4);
             }
             else{
-                questionLabel.setText("Zdobyte punkty: " +score);
+                switch (score){
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        questionLabel.setText("Ocena: 2.0 ");
+                        break;
+                    case 5:
+                    case 6:
+                        questionLabel.setText("Ocena: 3.0");
+                        break;
+                    case 7:
+                        questionLabel.setText("Ocena: 3.5");
+                        break;
+                    case 8:
+                        questionLabel.setText("Ocena: 4.0");
+                        break;
+                    case 9:
+                        questionLabel.setText("Ocena: 4.5");
+                        break;
+                    case 10:
+                        questionLabel.setText("Ocena: 5.0");
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + score);
+                }
                 rb1.setText("");
                 rb2.setText("");
                 rb3.setText("");
                 rb4.setText("");
+                result.setText("Zdobyte punkty: " +score);
                 submitButton.setText("Restart");
             }
         }
@@ -92,6 +125,10 @@ public class Controller {
             getNextQuestion(questions);
             submitButton.setText("Następne pytanie");
         }
+        rb1.setSelected(false);
+        rb2.setSelected(false);
+        rb3.setSelected(false);
+        rb4.setSelected(false);
 
     }
 }
