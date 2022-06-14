b7c0b998db38bfdc47e9a1dfbcdf4ca18c5d5d43
Hubert <hubertos120@gmail.com>
2022-06-14 18:42:19 +0200

    gui app

--- src/main/java/com/example/projekt_06_34319/Controller.java
+++ src/main/java/com/example/projekt_06_34319/Controller.java
@@ -3,7 +3,7 @@ package com.example.projekt_06_34319;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
